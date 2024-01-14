3b5caf312a4f3cacadcf875f90cf843454df5c50
Akash <akashindia572@gmail.com>
2024-01-14 10:53:50 +0530

    7th Class worklog

---
+++ Literals.java
@@ -0,0 +1,22 @@
+class Literals
+{
+
+public static void main(String[]args)
+{
+	//Integer Literals
+	System.out.println("1,2,3");
+
+	//Character Literals
+	System.out.println("'A','b','D'");
+
+	//String Literals
+	System.out.println("This is a String.");
+
+	//Boolean Literals
+	System.out.println("true/false");
+
+
+
+
+}
+}
---
+++ Variables.java
@@ -0,0 +1,30 @@
+class Variables
+{
+public static void main(String[]args)
+{
+//Variable Declearition
+int a;
+//Variable Initialisation.
+a=20;
+
+//Variable Declearition and Initialisation in a same line.
+
+int b= 30;
+//Variable Re-Initialisation.
+
+int c=40;
+
+c=69;
+
+//Variable Copy into another.
+
+int d = c;
+//Variable Print.
+ 
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+System.out.println(d);
+
+}
+}
