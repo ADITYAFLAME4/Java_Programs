0d61d096d5c256436810cff6e743c0934628b523
Akash <akashindia572@gmail.com>
2024-01-21 15:16:29 +0530

    New binary operator is implemented

---
+++ SUM.java
@@ -0,0 +1,24 @@
+class SUM
+{
+public static void main(String[]args)
+{
+int a,b;
+int sum;
+float div;
+float mod;
+int mul;
+int sub;
+
+a=8;
+b=4;
+
+sum= a+b;
+div=a/b;
+mod=a%b;
+mul=a*b;
+sub=a-b;
+
+System.out.println("Sum:" + sum + " "+ "Div:" + div + " "+ "Mod:"+ mod + " "+ "Mul:"+ mul+" "+ " "+ "SUB:"+ sub);
+
+}
+}
