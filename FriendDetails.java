138328d8d701a43e04e641023198f3ce40838c53
Akash <akashindia572@gmail.com>
2024-01-21 14:56:01 +0530

    Sir Just started java coding today!

---
+++ FriendDetails.java
@@ -0,0 +1,20 @@
+class FriendDetails
+{
+public static void main(String[]args)
+{
+String name="Rupam";
+int age= 23;
+String gender="Male";
+String address="Ashokenagar 102/3";
+float bankBalance = 9806000.67f;
+long phno=918348549937L;
+
+System.out.println(name);
+System.out.println(age);
+System.out.println(gender);
+System.out.println(address);
+System.out.println(bankBalance);
+System.out.println(phno);
+
+}
+}
---
+++ GLobalVariable.java
@@ -0,0 +1,13 @@
+class GlobalVariable
+{
+	static int n=10;
+public static void main(String[]args)
+{
+System.out.println(n);
+
+}
+
+
+
+
+}
