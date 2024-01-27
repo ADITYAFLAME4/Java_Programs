17efd8447982f78abf8a6055887baad4bd8a1205
Akash <akashindia572@gmail.com>
2024-01-27 15:57:03 +0530

    Method 2 try

---
+++ MethAddition.java
@@ -0,0 +1,15 @@
+class MethAddition
+{
+public static void main(String[]args)
+{
+addition();
+}
+
+public static void addition()
+{
+int a=5;
+int b=9;
+int sum= a+b;
+System.out.println(sum);
+}
+}
