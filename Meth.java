eac87cfca8766227d2a9c0b74e1d1cf7fa1d79fa
Akash <akashindia572@gmail.com>
2024-01-27 15:32:04 +0530

    Learning about java method

---
+++ Meth.java
@@ -0,0 +1,15 @@
+class Meth
+{
+public static void demo()
+{
+System.out.println("Hello");
+System.out.println("Hello Friends");
+System.out.println("GooD Bye!");
+}
+
+public static void main(String[]args)
+{
+	demo();
+}
+
+}
