9dfe5268765f6806c96b1d169262042702236d74
Akash <akashindia572@gmail.com>
2024-02-04 15:38:11 +0530

    String Concatination with Parameterized method with returntype

---
+++ StringCon.java
@@ -0,0 +1,16 @@
+class StringCon
+{
+public static String concati(String a1  , String a2 )
+{
+	String abc = a1 + a2 ;
+	return abc;
+}
+
+public static void main (String[]args)
+{
+	String container = concati("Rupam" , "Sayan");
+
+	System.out.println(container);
+}
+
+}
