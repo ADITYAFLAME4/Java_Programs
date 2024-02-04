ec641b54b7f1f8a8f1bdfc0d74711fe1774e063d
Akash <akashindia572@gmail.com>
2024-02-04 15:47:59 +0530

    Passing arguments (Integer Value) instead of float

--- CircleArea.java
+++ CircleArea.java
@@ -1,7 +1,7 @@
 class CircleArea
 {	
 	static double pi = 3.142;
-public static double circlearea( double rad)
+public static double circlearea(int rad)
 {
 	
 	double res = pi*rad*rad;
@@ -10,7 +10,7 @@ public static double circlearea( double rad)
 
 public static void main(String[]args)
 {
-	double area = circlearea(5.89);
+	double area = circlearea(8);
 
 	System.out.println("The area of circle is:" + area);
 }
