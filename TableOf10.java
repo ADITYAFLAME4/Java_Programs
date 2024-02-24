15cf19067f5104d96778f356a28856d63211a7a6
Akash <akashindia572@gmail.com>
2024-02-24 17:21:28 +0530

    TableOf10

---
+++ TableOf10.java
@@ -0,0 +1,11 @@
+public class TableOf10 {
+        public static void main(String[] args) {
+            int i = 1;
+            while( i <=10)
+            {
+                System.out.println("10" + "x" + i + "=" + (10*i));
+                i++;
+            }
+        }
+    }
+    
